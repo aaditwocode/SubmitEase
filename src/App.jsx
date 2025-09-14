0cde123e6a1d9c3dd7582168e572e6ce03e77df6
Aaditya Pratap Singh <aaditwocode@gmail.com>
2025-09-14 17:52:11 +0530

    Tailwind importation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Header from "./components/Header";
+
+
+
 
 function App() {
- 
+  return (
+  
+      <Header />
+     
+  );
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='px-30 bg-amber-400'>Header</div>
+  )
+}
+
+export default Header
